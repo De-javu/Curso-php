931d158cdfd8e1459e50ad1b387448834adb26e8
andres pardo <andrespardo5151@gmail.com>
2020-08-17 17:04:31 -0500

    feat: implementacion de require e include des Please enter the commit message for your changes. Lines starting

--- index.php
+++ index.php
@@ -1,46 +1,13 @@
 <?php
+
+require_once('jobs.php');
+
 $name2= 'Andres';
 $lastName = 'Pardo';
 $name = "Carlos $name2 $lastName";
 $limitmonths = 2000;
 
-$jobs = [
- 
-  [
-    'title' => 'PHP Devoloper',
-    'description' => 'this is an awasome job!!!',
-    'visible' => true,
-    'months' => 16
-   ],
 
-  [
-    'title' => 'Python Dev',
-    'description' => 'best experience!!!',
-    'visible' => false,
-    'months' => 14  ],
-
-  [
-    'title' => 'Devops',
-    'description' => 'best experience!!!',
-    'visible' => false,
-    'months' => 5
-  ],
-
-  [
-    'title' => 'Node Dev',
-    'description' => 'best experience!!!',
-    'visible' => true,
-    'months' => 24
-  ],
-
-  [
-    'title' => 'Frontend Dev',
-    'description' => 'best experience!!!',
-    'visible' => false,
-    'months' => 3
-  ]
-
-  ];
 
   function getDuration($months){
     $years = floor($months / 12);
---
+++ jobs.php
@@ -0,0 +1,39 @@
+<?php
+
+$jobs = [
+ 
+    [
+      'title' => 'PHP Devoloper',
+      'description' => 'this is an awasome job!!!',
+      'visible' => true,
+      'months' => 16
+     ],
+  
+    [
+      'title' => 'Python Dev',
+      'description' => 'best experience!!!',
+      'visible' => false,
+      'months' => 14  ],
+  
+    [
+      'title' => 'Devops',
+      'description' => 'best experience!!!',
+      'visible' => false,
+      'months' => 5
+    ],
+  
+    [
+      'title' => 'Node Dev',
+      'description' => 'best experience!!!',
+      'visible' => true,
+      'months' => 24
+    ],
+  
+    [
+      'title' => 'Frontend Dev',
+      'description' => 'best experience!!!',
+      'visible' => false,
+      'months' => 3
+    ]
+  
+    ];
