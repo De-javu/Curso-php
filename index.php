4b02637c11584177ad451354b3b5af6318ea73b1
andres pardo <andrespardo5151@gmail.com>
2020-08-22 11:58:35 -0500

    feat: Herencia

--- index.php
+++ index.php
@@ -72,18 +72,30 @@ $limitmonths = 2000;
                  break;
                  }
  
-               printJob($jobs[$idx]);
-                
+               printElement($jobs[$idx]);  
                 
                }
-            
-              
+                          
               ?>
            
           </ul>
         </div>
         <div>
             <h3 class="border-bottom-gray">Projects</h3>
+
+            <ul>
+          <?php
+               for($idx = 0;$idx < count($projects); $idx++){
+                 
+ 
+               printElement($projects[$idx]); 
+                
+               }
+                          
+              ?>
+           
+          </ul>
+
             <div class="project">
                 <h5>Project X</h5>
                 <div class="row">
--- jobs.php
+++ jobs.php
@@ -1,47 +1,10 @@
 <?php
 
-class Job {
-    private $title;
-    public $description;
-    public $visible = true;
-    public $months;
-
-    public function __construct($title, $description)
-    {
-      $this->setTitle =$title;
-      $this->description = $description;
-    }
-
-  public function setTitle($t){
-    if($t == '') {
-           $this->title = 'N/A';
-    } else {
-           $this->title = $t;
-    }
-  }
-    
-
-   public  function getTitle() {
-
-     return $this->title;
-
-  }
-
-  public function getDurationAsString(){
-    $years = floor($this->months / 12);
-    $extraMonths = $this->months % 12;
-
-
-    return " $years years $extraMonths months";
-
-
-  }
-
-}
+require  'app/Models/Job.php';
+require  'app/Models/Project.php';
 
 $job1 = new Job('PHP Devoloper','this is an awasome job!!!');
 
-$job1->visible = true;
 $job1->months = 16;
 
 $job2 = new Job('Python Dev', 'this is an awasome job!!!' );
@@ -51,51 +14,22 @@ $job3 = new Job('', 'this is an awasome job!!!' );
 
 $job3->months = 32;
 
+ $project1 = new Project('Project 1', 'Description 1');
+
 $jobs = [
  
        $job1,
        $job2,
-       $job3
-    
-
-    // [
-    //   'title' => 
-    //   'description' => 'this is an awasome job!!!',
-    //   'visible' => true,
-    //   'months' => 16
-    //  ],
-  
-    // [
-    //   'title' => 'Python Dev',
-    //   'description' => 'best experience!!!',
-    //   'visible' => false,
-    //   'months' => 14  ],
-  
-    // [
-    //   'title' => 'Devops',
-    //   'description' => 'best experience!!!',
-    //   'visible' => false,
-    //   'months' => 5
-    // ],
-  
-    // [
-    //   'title' => 'Node Dev',
-    //   'description' => 'best experience!!!',
-    //   'visible' => true,
-    //   'months' => 24
-    // ],
-  
-    // [
-    //   'title' => 'Frontend Dev',
-    //   'description' => 'best experience!!!',
-    //   'visible' => false,
-    //   'months' => 3
-    // ]
+       $job3  
   
      ];
+
+    $projects = [
+      $project1
+    ];
      
   
-     function printJob($job) {
+     function printElement($job) {
        
       if($job->visible == false) { 
         return;
